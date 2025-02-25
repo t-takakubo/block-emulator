8babf5bc89ff49c55f452a9ca3db8f154595a05f
Toshiki Takakubo <219tosiki@gmail.com>
2025-02-25 20:05:53 +0900

    feat: add batch scripts for running application

---
+++ complie_run_IpAddr=127_0_0_1.sh
@@ -0,0 +1,10 @@
+go run main.go -n 0 -N 1 -s 0 -S 4 & 
+
+go run main.go -n 0 -N 1 -s 1 -S 4 & 
+
+go run main.go -n 0 -N 1 -s 2 -S 4 & 
+
+go run main.go -n 0 -N 1 -s 3 -S 4 & 
+
+go run main.go -c -N 1 -S 4 & 
+
